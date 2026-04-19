fca8ce21911220d920ce91fda95b7850b75b43ab
CryslaineGomes <104288350+CryslaineGomes@users.noreply.github.com>
2026-04-18 22:21:48 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+function scrollToImoveis() {
+  document.getElementById("imoveis").scrollIntoView({
+    behavior: "smooth"
+  });
+}
 
+function whatsapp() {
+  window.open("https://wa.me/5599999999999", "_blank");
+}
